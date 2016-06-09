ecb9548d45f0e6e47c6de31312e94e829b93fd7e
Kionte <kbrown17@my.whitworth.edu>
2016-06-08 20:00:17 -0700

    Unit Testing Tutorial

---
+++ TutUnitTesting/TutUnitTesting/Square.cpp
@@ -0,0 +1,13 @@
+#include "Square.h"
+
+bool isSquare(int a, int b, int c, int d) {
+
+	// A correct function would return true if a, b, c, d are valid sides for a square
+//	if (a == 0 && b == 0 && c == 0 && d == 0)
+	//	return false;
+	if(a == b && a == c && a == d && a > 0) 
+		return true;
+
+	return false;
+
+}
---
+++ TutUnitTesting/UnitTester/unittest1.cpp
@@ -0,0 +1,94 @@
+#include "stdafx.h"
+
+#include "CppUnitTest.h"
+
+#include "Triangle.h" // This must GO AFTER CppUnitTest.h!!!!
+#include "../TutUnitTesting/Triangle.h"
+#include <Square.h>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTester
+
+{
+
+	TEST_CLASS(TriangleTest) // This our test suite for testing isTriangle()
+
+	{
+
+	public:
+
+		TEST_METHOD(AllDifferent) // Test case 1: all sides different
+
+		{
+
+			// We are checking that isTriangle() should return true
+
+			Assert::AreEqual(isTriangle(3, 4, 5), true);
+
+		}
+
+		TEST_METHOD(TooLong) // Test case 2: one side is too long
+
+		{
+
+			// We are checking that isTriangle() should return false
+
+			Assert::AreEqual(isTriangle(3, 3, 7), false);
+
+		}
+
+		TEST_METHOD(Zeros) // Test case 3: all sides zero
+
+		{
+
+			// We are checking that isTriangle() should return false
+
+			Assert::AreEqual(isTriangle(0, 0, 0), false);
+
+		}
+
+		TEST_METHOD(Negatives) // Test case 4: all sides negative
+
+		{
+
+			// We are checking that isTriangle() should return false
+
+			Assert::AreEqual(isTriangle(-1, -1, -1), false);
+
+		}
+
+	};
+	TEST_CLASS(SquareTest) // Test suite for testing isSquare()
+
+	{
+
+		TEST_METHOD(SquareZeros) // Test case 1: all sides are zero
+
+		{
+
+			Assert::AreEqual(isSquare(0, 0, 0, 0), false);
+
+		}
+		TEST_METHOD(SquarenEqual)
+		{
+			Assert::AreEqual(isSquare(1, 2, 3, 4), false);
+		}
+		TEST_METHOD(SquareNegative)
+		{
+			Assert::AreEqual(isSquare(1, 1, 1, -1), false);
+		}
+		TEST_METHOD(SquareisEqual)
+		{
+			Assert::AreEqual(isSquare(2, 2, 2, 2), true);
+		}
+		TEST_METHOD(SquareAllNeg)
+		{
+			Assert::AreEqual(isSquare(-1, -1, -1, -1), false);
+		}
+
+		// Add your additional test cases here!
+
+	};
+
+}
