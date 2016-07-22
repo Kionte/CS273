73c21f184537564a7191004c887265b91f5a688b
Kionte <kbrown17@my.whitworth.edu>
2016-07-22 11:15:27 -0700

    a few comments
    about what classes i need to add

--- MyFinalProject/MyFinalProject/Source.cpp
+++ MyFinalProject/MyFinalProject/Source.cpp
@@ -1,6 +1,9 @@
 #include "EmergencyRoom.h"
 
 EmergencyRoom sacredHeart;
+// need to add nurse class
+// need to add doctor class
+// need to add caregiver abs class
 
 int main() {
 	sacredHeart.enterInfo();
